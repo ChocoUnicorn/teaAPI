1e918b30c6f5d9835b627afe62642925de9c84ee
ChocoUnicorn <eokoloani@yandex.com>
2022-06-05 23:25:16 +0100

    More tea types plus images in database

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const tea = {
         'waterTemp': 80,
         'steepTime': 200,
         'caffeinated': true,
-        'flavour': 'strong'
+        'flavour': 'strong',
+        'isHerbal': true,
+        'image':'https://images.pexels.com/photos/6545346/pexels-photo-6545346.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
     'macha': {
         'type': 'green',
@@ -21,7 +23,9 @@ const tea = {
         'waterTemp': 90,
         'steepTime': 200,
         'caffeinated': true,
-        'flavour': 'very strong'
+        'flavour': 'very strong',
+        'isHerbal': true,
+        'image':'https://images.pexels.com/photos/461428/pexels-photo-461428.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
 
     'black': {
@@ -30,7 +34,63 @@ const tea = {
         'waterTemp': 90,
         'steepTime': 200,
         'caffeinated': true,
-        'flavour': 'strong'
+        'flavour': 'strong',
+        'isHerbal': false,
+        'image':'https://images.pexels.com/photos/1493080/pexels-photo-1493080.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+
+    'charmomile': {
+        'type': 'yellow',
+        'origin': 'china',
+        'waterTemp': 80,
+        'steepTime': 200,
+        'caffeinated': false,
+        'flavour': 'mild',
+        'isHerbal': true,
+        'image':'https://cdn.mos.cms.futurecdn.net/Fi4jRrhVKLFzZSkgdqbbKF.jpg'
+    },
+
+    'raspberry': {
+        'type': 'red',
+        'origin': 'china',
+        'waterTemp': 80,
+        'steepTime': 200,
+        'caffeinated': false,
+        'flavour': 'mild',
+        'isHerbal': true,
+        'image':'https://www.mysequinedlife.com/wp-content/uploads/2016/08/iced-raspberry-mint-green-tea-3.jpg'
+    },
+    'green': {
+        'type': 'green',
+        'origin': 'china',
+        'waterTemp': 90,
+        'steepTime': 200,
+        'caffeinated': true,
+        'flavour': 'medium',
+        'isHerbal': true,
+        'image':'https://images.pexels.com/photos/1793035/pexels-photo-1793035.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+
+    'earlgrey': {
+        'type': 'black',
+        'origin': 'china',
+        'waterTemp': 90,
+        'steepTime': 200,
+        'caffeinated': true,
+        'flavour': 'medium',
+        'isHerbal': false,
+        'image':'https://www.billi-uk.com/wp-content/uploads/2017/09/Hetties-Earl-Grey-1024x683.jpg'
+    },
+
+    'darjeeling': {
+        'type': 'black',
+        'origin': 'china',
+        'waterTemp': 90,
+        'steepTime': 200,
+        'caffeinated': true,
+        'flavour': 'strong',
+        'isHerbal': false,
+        'image':'http://teafloor.com/blog/wp-content/uploads/2018/11/darjeeling-tea1.png'
     },
 
     'unknown': {
@@ -39,7 +99,9 @@ const tea = {
         'waterTemp': 'unknown',
         'steepTime': 'unknown',
         'caffeinated': 'unknown',
-        'flavour': 'unknown'
+        'flavour': 'unknown',
+        'isHerbal': 'unknown',
+        'image': 'https://www.pexels.com/photo/silver-round-accessory-with-storage-227908/'
     }
 }
 
@@ -54,7 +116,7 @@ app.get('/api', (request, response) => {
 })
 
 app.get('/api/:name', (request, response) => {
-    let teaName = request.params.name.toLowerCase()
+    let teaName = request.params.name.toLowerCase().split(' ').join('')
     if (tea[teaName]){
         response.json(tea[teaName])
     } else {
